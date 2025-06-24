25c1a3bef5492d9d93b8c54ed19f2c7096e82c9d
01Nimantha <167612890+01Nimantha@users.noreply.github.com>
2025-06-24 22:20:26 +0530

    create Lecture04

---
+++ Lecture04 - Taking User Input/app.js
@@ -0,0 +1,39 @@
+const http = require("http");
+const fs = require("fs");
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>My API in NODE</title></head>");
+    res.write("<body><h1>Enter your Details.</h1>");
+    res.write(
+      '<form action="/details" method="POST"><input type="text" placeholder="Enter your Name" name="name"/> <br/>'
+    );
+    res.write("<h2>Select Gender</h2>");
+    res.write('Male<input type="radio" name="gender" value="male"/> <br/>');
+    res.write(
+      'Female<input type="radio" name="gender" value="female"/> <br/><br/><br/>'
+    );
+    res.write('<button type="submit">Submit</button>');
+    res.write("</form></body></html>");
+    return res.end();
+  } else if (
+    req.url.toLocaleLowerCase() === "/details" &&
+    req.method == "POST"
+  ) {
+    fs.writeFileSync("details.txt", "Nimantha");
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+  }
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My API in NODE</title></head>");
+  res.write("<body><h1>Welcome to the Page.</h1></body>");
+  res.write("</html>");
+  res.end();
+});
+
+const port = 3000;
+server.listen(port, "0.0.0.0", () => {
+  console.log(`Server running on address http://localhost:${port}`);
+});
